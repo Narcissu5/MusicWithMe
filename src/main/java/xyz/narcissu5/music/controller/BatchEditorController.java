e91baa3e11f0da93ea3d20d35311938932b8bd50
曾浩 <ch269632@alibaba-inc.com>
2022-03-08 09:36:26 +0800

    增加推断方式

--- src/main/java/xyz/narcissu5/music/controller/BatchEditorController.java
+++ src/main/java/xyz/narcissu5/music/controller/BatchEditorController.java
@@ -2,6 +2,8 @@ package xyz.narcissu5.music.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import xyz.narcissu5.music.model.Song;
 
@@ -17,6 +19,12 @@ public class BatchEditorController extends PopupBaseController {
     @FXML
     TextField album;
 
+    @FXML
+    CheckBox fromFileName;
+
+    @FXML
+    ComboBox<String> inferWay;
+
     public void setSongs(Collection<Song> songs) {
         this.songs = songs;
     }
@@ -28,15 +36,30 @@ public class BatchEditorController extends PopupBaseController {
     public void save(ActionEvent event) {
         String artists = this.artists.getText();
         String album = this.album.getText();
-        if ((artists == null || "".equals(artists)) && (album == null || "".equals(album))) {
+        if ((artists == null || "".equals(artists)) &&
+                (album == null || "".equals(album)) &&
+                !fromFileName.isSelected()) {
             return;
         }
         for (Song song : songs) {
             if (artists != null && !"".equals(artists)) {
                 song.artists.set(artists);
             }
-            if(album!= null && !"".equals(album)) {
+            if (album != null && !"".equals(album)) {
                 song.album.set(album);
+            }
+            if(fromFileName.isSelected()) {
+                String filename = song.getFile().getFile().getName();
+                filename = filename.substring(0, filename.lastIndexOf('.'));
+                String[] tokens = filename.split("-");
+                String inferWayStr = inferWay.getSelectionModel().getSelectedItem();
+                if("歌手-歌名".equals(inferWayStr)) {
+                    song.artists.set(tokens[0]);
+                    song.title.set(tokens[1]);
+                } else if ("歌名-歌手".equals(inferWayStr)) {
+                    song.title.set(tokens[0]);
+                    song.artists.set(tokens[1]);
+                }
             }
             song.commit();
         }
